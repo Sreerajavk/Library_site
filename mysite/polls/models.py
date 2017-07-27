b017d1b7c4b0b0713662f856a293c570f649e452
Sreerajavk <sreerajavk123@gmail.com>
2017-07-27 19:22:09 +0530

    Add files via upload

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,13 +8,14 @@ from django.contrib.auth.models import User
 class details(models.Model):
     Name = models.CharField(max_length = 100)
     Age =  models.IntegerField()
-    dob = models.DateField()
+    dob = models.DateTimeField()
 
     def __str__(self):
         return self.Name
 
 class userprofile(models.Model):
     User = models.OneToOneField(User)
+    Name = models.OneToOneField(details)
     city = models.CharField(max_length = 100)
 
         
